67b5895b03d66ed95dd88157f6d80aaafb9ea23f
horang100 <goheekwon9991@gmail.com>
2025-11-10 16:11:43 +0900

    first commit
    first time git commit

---
+++ chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,19 @@
+package sec03.exam01;
+//1. 명언. 잘 짜여진 코드가 곧 주석이다.
+
+/*
+	ctrl + shift + / 
+*/
+//	ctrl + /
+
+//TODO: Task 뷰 추가해서 확인
+
+public class Hello { //클래스 선언, 첫 글자는 반드시 upper case, src파일 이름과 동일해야함. Hello.java
+	// 프로그램 실행 진입점(EP:entry point)
+	// main 메소드: 자바 프로그램 실행 시 가장 먼저 실행되는 메소드
+	public static void main(String[] args) {
+		System.out.println("Hello,// /**/ () for()Java"); //문자열 내부 주석 사용 불가.
+		
+	}
+
+}
