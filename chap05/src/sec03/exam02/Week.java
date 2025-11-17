528a100ab76224902910565cd7a8b133ba982925
horang100 <goheekwon9991@gmail.com>
2025-11-17 14:10:44 +0900

    test1

---
+++ chap05/src/sec03/exam02/Week.java
@@ -0,0 +1,16 @@
+package sec03.exam02;
+
+public enum Week {
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+
+//	public static final Week MONDAY = new Week("MONDAY", 0);
+}
+
+//(참고) 컴파일 시점에 자동으로 public static final 특성을 갖는 상수로 생성
+//상수이므로 메소드 영역에 저장됨.
